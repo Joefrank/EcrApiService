5163de2d54534cb23ee713c869b08207a8c46c2c
unknown <admin@cyberminds.co.uk>
2015-09-30 00:54:22 +0100

    created solution

---
+++ EcrApiTest/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace EcrApiTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            
+            EcrApiService.TourAvailability service = new EcrApiService.TourAvailability();
+            
+            EcrApiService.Api client = new EcrApiService.Api();
+            var tourlist = client.TourList("1q-psWDffplAr1fgJAX_HVfH36eCs3z9MqXeM-hWHao");
+            var availability = client.Availability("1q-psWDffplAr1fgJAX_HVfH36eCs3z9MqXeM-hWHao", "01011", DateTime.Now.AddDays(-100), true, DateTime.Now.AddDays(100), true);
+
+
+
+            return View();
+        }
+
+    }
+}
---
+++ EcrApiTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EcrApiTest.MvcApplication" Language="C#" %>
